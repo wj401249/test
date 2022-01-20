2582f5cb7311c0ea19848adfa73e6e131e76f815
Wj <314047179@163.com>
2022-01-20 09:18:53 +0800

    add view provider

---
+++ app/Providers/ViewProvider.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace demo\Providers;
+
+use Phalcon\Di\DiInterface;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Mvc\View;
+
+class ViewProvider implements ServiceProviderInterface
+{
+    protected string $providerName = 'view';
+
+    public function register(DiInterface $di) : void
+    {
+        $viewDir = $di->offsetGet('rootPath') . '/app/Views';
+        $di->setShared($this->providerName, function() use ($viewDir) {
+            $view = new View();
+            $view->setViewsDir($viewDir);
+            return $view;
+        });
+    }
+}
